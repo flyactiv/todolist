e9352095171af0e819d39699f9c5a5dfd8804c6c
flyactiv <flyactiv46@gmail.com>
2022-03-12 16:22:14 +0300

    Create a user model

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,6 @@
 from exts import db
 
-
+#recipe model
 """
 class Recipe:
     id:int primary_key
@@ -31,3 +31,24 @@ class Recipe(db.Model):
         self.description = description
 
         db.session.commit()
+
+
+
+#user model
+
+"""
+class User:
+    id:integer
+    username:string
+    email:string
+    password:string
+"""
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25),nullable=False, unique=True)
+    email = db.Column(db.String(80),nullable=False)
+    password = db.Column(db.Text(),nullable=False)
+
+    def __repr__(self):
+        return f"<User {self.username}>"
