1aceb7c884c0b4f3fb1d012f99768643dd0fa384
flyactiv <flyactiv46@gmail.com>
2022-03-12 18:55:32 +0300

    Create login and signup routes and password hashing

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,10 @@
-from flask import Flask, request
+from flask import Flask, requestm, jsonify
 from flask_restx import Api, Resource,fields
 from config import DevConfig
-from models import Recipe
+from models import Recipe, User
 from exts import db
 from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
@@ -25,12 +26,55 @@ recipe_model=api.model(
     }
 )
 
+
+
+
+signup_model=api.model(
+    'SignUp',
+    {
+        username:fields.String(),
+        email:fields.String(),
+        password:fields.String()
+    }
+)
+
+
 @api.route('/hello')
 class HelloResource(Resource):
     def get(self):
         return {"message":"Hello World"}
 
 
+
+@api.route('/signup')
+class SignUp(Resource):
+    @api.marshal_with(signup_model)
+    @api.expect(signup_model)
+    def post(self):
+        data=request.get_json()
+
+        username=data.get('username')
+
+        db_user=User.query.filter(username=username).first()
+
+        if db_user is not None:
+            return jsonify({"message":f"User with username{username} already exists"})
+
+        new_user=User(
+            username=data.get('username'),
+            email=data.get('email'),
+            password=generate_password_hash(data.get('password'))
+            )
+
+        new_user.save()
+
+        return new_user,201
+
+
+@api.route('/login')
+class Login(Resource):
+    def post(self):
+        pass
 
 @api.route('/recipes')
 class RecipeResource(Resource):
@@ -45,6 +89,7 @@ class RecipeResource(Resource):
 
 
     @api.marshal_with(recipe_model)
+    @api.expect(recipe_model)
     def post(self):
         """Create a new recipe"""
 
