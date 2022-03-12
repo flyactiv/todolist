4eac2a8db55a085ab3bed7e744921d0cf4d26b8c
flyactiv <flyactiv46@gmail.com>
2022-03-12 15:32:25 +0300

    Create a resource and get a resource with INSOMNIA

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from flask_restx import Api, Resource 
+from flask import Flask, request
+from flask_restx import Api, Resource,fields
 from config import DevConfig
 from models import Recipe
 from exts import db
@@ -11,10 +11,66 @@ db.init_app(app)
 
 api=Api(app,doc='/docs')
 
+#model (serializer)
+recipe_model=api.model(
+    "Recipe",
+    {
+        "id":fields.Integer(),
+        "title":fields.String(),
+        "description":fields.String()
+    }
+)
+
 @api.route('/hello')
 class HelloResource(Resource):
     def get(self):
         return {"message":"Hello World"}
+
+
+
+@api.route('/recipes')
+class RecipeResource(Resource):
+
+    @api.marshal_list_with(recipe_model)
+    def get(self):
+        """Get all recipes"""
+        recipes=Recipe.query.all()
+
+
+        return recipes
+
+
+    @api.marshal_with(recipe_model)
+    def post(self):
+        """Create a new recipe"""
+
+
+        data=request.get_json()
+
+        new_recipe=Recipe(
+            title=data.get('title'),
+            description=data.get('description'),
+        )
+
+        new_recipe.save()
+
+
+        return new_recipe,201
+
+
+@api.route('/recipe/<int:id>')
+class RecipeResource(Resource):
+    def get(self, id):
+        """Get a recipe by id"""
+        pass
+
+    def put(self, id):
+        """Update a recipe by id"""
+        pass
+
+    def delete(self, id):
+        """Delete a recipe by id"""
+        pass
 
 
 @app.shell_context_processor
