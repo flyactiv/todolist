1ed2cd1ab03d948bb65c817adf79ee198dcdcdec
flyactiv <flyactiv46@gmail.com>
2022-03-30 13:32:39 +0300

    React routing

---
+++ client/src/components/SignUp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignUpPage=()=>{
+    return(
+        <div className="signUp">
+            <h1>SignUp Page</h1>
+        </div>
+    )
+}
+
+export default SignUpPage
--- client/src/index.js
+++ client/src/index.js
@@ -1,28 +1,44 @@
-import React, {useEffect,useState} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react'
 import ReactDOM from 'react-dom'
+import Navbar from './components/Navbar';
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from 'react-router-dom'
+import CreateRecipePage from './components/CreateRecipes';
+import LoginPage from './components/Login';
+import SignUpPage from './components/SignUp';
+import HomePage from './components/Home';
 
 
 const App=()=>{
 
-    useEffect(
-        ()=>{
-            fetch('/recipe/hello')
-            .then(response=>response.json())
-            .then(data=>{console.log(data)
-            
-                setMessage(data.message)
-            
-            })
-            .catch(err=>console.log(err))
-
-            
-        },[]
-    )
-    const [message, setMessage]=useState('')
+    
+    
     return (
-        <div className="app">
-            <h1>{message}</h1>
-        </div>
+        <Router>
+            <div className="container">
+                <Navbar/>
+                <Switch>
+                    <Route path="/signup">
+                        <SignUpPage/>
+                    </Route>
+                    <Route path="/login">
+                        <LoginPage/>
+                    </Route>
+                    <Route path="/create-recipe">
+                        <CreateRecipePage/>
+                    </Route>
+                    <Route path="/">
+                        <HomePage/>
+                    </Route>
+
+                </Switch>
+            </div>
+        </Router>
     )
 }
 
