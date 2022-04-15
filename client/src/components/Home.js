ebd833eaac8d21de7342c975dab65d1bee30c4df
Flyactiv <flyactiv46@gmail.com>
2022-04-15 23:30:15 +0300

    Client-side JWT auth with React

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useAuth } from '../auth'
 
-const HomePage=()=>{
+
+const LoggedinHome = () => {
     return(
+        <div className='recipes'>
+            <h1>List of Todos</h1>
+        </div>
+    )
+}
+
+
+const LoggedOutHome = () => {
+    return (
         <div className="home container">
-            <h1 className="heading">Welcome to the To do list</h1>
+            <h1 className="heading">Welcome to the Recipes</h1>
             <Link to='/signup' className="btn btn-primary btn-lg">Get Started</Link>
+        </div>
+    )
+}
+
+const HomePage = () => {
+
+    const [logged] = useAuth()
+
+    return (
+        <div>
+            {logged ? <LoggedinHome /> : <LoggedOutHome />}
         </div>
     )
 }
