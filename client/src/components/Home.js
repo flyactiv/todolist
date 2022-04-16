c2fd945868ae26679c31774981a36e0d4a19542e
Flyactiv <flyactiv46@gmail.com>
2022-04-16 11:07:55 +0300

    Getting all api resources

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../auth'
+import {Recipe} from './Recipe'
 
 
 const LoggedinHome = () => {
+
+    const [recipes, setRecipes] = useState([]);
+
+
+    useEffect(
+        () => {
+            fetch('/recipe/recipes')
+                .then(res => res.json())
+                .then(data => {
+                    setRecipes(data)
+                })
+                .catch(err => console.log(err))
+        }, []
+    );
+
+
+
+
     return(
         <div className='recipes'>
             <h1>List of Todos</h1>
+            {
+                recipes.map(
+                    ()=>(
+                        <Recipe title ={recipe.title} description={recipe.description}/>
+                    )
+                )
+            }
         </div>
     )
 }
@@ -15,7 +41,7 @@ const LoggedinHome = () => {
 const LoggedOutHome = () => {
     return (
         <div className="home container">
-            <h1 className="heading">Welcome to the Recipes</h1>
+            <h1 className="heading">Welcome to the Todos</h1>
             <Link to='/signup' className="btn btn-primary btn-lg">Get Started</Link>
         </div>
     )
